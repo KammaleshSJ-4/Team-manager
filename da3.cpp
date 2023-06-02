988d5ddc431d030bde7f9c55243559adaa4e17bf
KammaleshSJ-4 <133968318+KammaleshSJ-4@users.noreply.github.com>
2023-06-02 17:52:29 +0530

    da3.cpp

--- da3.cpp
+++ da3.cpp
@@ -10,8 +10,9 @@ void solve(vector<int> A,vector <string> t){
    int i = max_element(A.begin(), A.end()) - A.begin();
    sort(A.begin(), A.end());
    int l = A[n - 1];
-   cout<< l<<endl;
-   cout<<t[i-5]<<endl;
+   
+   cout<< "Maximum amount "<<l<<endl;
+   cout<<"Bought by team"<<t[i-5]<<endl;
 }
 //player class
 class player{
@@ -35,6 +36,7 @@ class batsman:public player{
         aver=runs/matches;
         str_rate=runs/balls_faced;
     }
+    
     void print_bat{
         cout<<first_name<<endl;
         cout<<last_name<<endl;
@@ -63,7 +65,7 @@ class bowler:public player{
         cout<<first_name<<endl;
         cout<<last_name<<endl;
         cout<<nation<<endl;
-        cout<<type_bat<<endl;
+        cout<<type_bowl<<endl;
         cout<<matches<<endl;
         cout<<wickets<<endl;
         cout<<overs<<endl;
@@ -76,6 +78,20 @@ class bowler:public player{
 class allround:public player,public batsman,public bowler{
     public:
     int catches;
+    void print_allround{
+    	cout<<first_name<<endl;
+        cout<<last_name<<endl;
+        cout<<nation<<endl;
+        cout<<type_bat<<endl;
+        cout<<matches<<endl;
+        cout<<runs<<endl;
+        cout<<aver<<endl;
+        cout<<str_rate<<endl;
+        cout<<type_bowl<<endl;
+        cout<<matches<<endl;
+        cout<<wickets<<endl;
+        cout<<overs<<endl;
+        cout<<economy<<endl;
 };
 
 
@@ -85,18 +101,33 @@ class allround:public player,public batsman,public bowler{
 int main(){
     //vector bids
    vector<int> A(5);
-   vector<string> team{"Chen", "Mum","Kol","Del","Ben"};
+   vector<string> team{"Chennai", "Mumbai","Kolkata","Delhi","Bengalore"};
  
  
   // Print Strings stored in Vector
+  cout<<"The teams are:"<<endl;
    for (int i = 0; i < team.size(); i++){
        std::cout <<team[i] << "\n";}
-    
+   string player_type;
+   cin>>player_type;
+   if (player_type==" BATSMAN") {
+       batsman p1;
+       p1.get_bat();
+       p1.calc_bat() ;
+       p1.print_bat() ;
+   else if (player_type=="BOWLER") {
+      bowler p1;
+      p1.get_bowl;
+      p1.calc_bowl;
+      p1.print_bowl;
+   
 
    int a;
+   cout<<"Please provide the bids of the player"<<endl;
    for (int j=0;j<5;j++){
        cin>>a;
        A.push_back(a);
    }
    solve(A,team);
+   
 }
